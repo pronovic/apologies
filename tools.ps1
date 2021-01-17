df09f34600c45a64a4416ae5af98e43a6a0d68b9
Kenneth J. Pronovici <pronovic@ieee.org>
2021-01-17 10:59:41 -0600

    Tell Pylint to parallelize its scan

--- tools.ps1
+++ tools.ps1
@@ -27,7 +27,7 @@ Switch ($command)
 
     pylint {
       Write-Output "Running pylint checks..." 
-      poetry run pylint src/apologies tests
+      poetry run pylint -j 0 src/apologies tests
     }
 }
 
