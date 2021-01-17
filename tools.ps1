ddcb1c4cb3ae40a190f54f2db7abc6f921f00ed9
Kenneth J. Pronovici <pronovic@ieee.org>
2021-01-17 10:48:18 -0600

    Move in-scope files to .mypyrc

--- tools.ps1
+++ tools.ps1
@@ -22,7 +22,7 @@ Switch ($command)
 
     mypy {
       Write-Output "Running mypy checks..." 
-      poetry run mypy --config-file=.mypyrc src/apologies tests
+      poetry run mypy --config-file=.mypyrc
     }
 
     pylint {
