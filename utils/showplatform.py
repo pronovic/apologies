7253024ca5c4f328ab29f3dec170b16fc0320048
Kenneth J. Pronovici <pronovic@users.noreply.github.com>
2022-09-02 16:58:30 -0500

    Add caching in GitHub Actions workflow (#40)

--- utils/showplatform.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Display information about the current platform
-
-import platform
-import sys
-
-fields = ["   %s: %s" % i for i in zip(["system", "node", "release", "version", "machine", "processor"], platform.uname()) if i[1]]
-print("Python %s" % sys.version)
-print("Platform\n%s" % "\n".join(fields))
