3311f9431ade4cd6d3485781098e09db6ca3034a
Kenneth J. Pronovici <pronovic@ieee.org>
2022-09-02 16:16:33 -0500

    More debugging on Windows

--- utils/poetrydirs.sh
+++ utils/poetrydirs.sh
@@ -9,17 +9,18 @@
 if [ "$GITHUB_ACTIONS" == "true" ]; then
 
    if [ "$RUNNER_OS" == "Linux" ]; then
-      echo "POETRY_INSTALL=/home/runner/.local" >> "$GITHUB_ENV"
-      echo "POETRY_CONFIG=/home/runner/.config/pypoetry" >> "$GITHUB_ENV"
-      echo "$POETRY_INSTALL/bin" >> "$GITHUB_PATH"
+      POETRY_INSTALL="/home/runner/.local"
+      POETRY_CONFIG="/home/runner/.config/pypoetry"
    elif [ "$RUNNER_OS" == "macOS" ]; then
-      echo "POETRY_INSTALL=/Users/runner/.local" >> "$GITHUB_ENV"
-      echo "POETRY_CONFIG=/Users/runner/Library/Preferences/pypoetry" >> "$GITHUB_ENV"
+      POETRY_INSTALL="/Users/runner/.local"
+      POETRY_CONFIG="/Users/runner/Library/Preferences/pypoetry"
    elif [ "$RUNNER_OS" == "Windows" ]; then
-      echo "POETRY_INSTALL=C:/Users/runneradmin/AppData/Roaming/Python/Scripts" >> "$GITHUB_ENV"
-      echo "POETRY_CONFIG=C:/Users/runneradmin/AppData/Roaming/pypoetry" >> "$GITHUB_ENV"
+      POETRY_INSTALL="C:/Users/runneradmin/AppData/Roaming/Python/Scripts"
+      POETRY_CONFIG="C:/Users/runneradmin/AppData/Roaming/pypoetry"
    fi 
 
+   echo "POETRY_INSTALL=$POETRY_INSTALL" >> "$GITHUB_ENV"
+   echo "POETRY_CONFIG=$POETRY_CONFIG" >> "$GITHUB_ENV"
    echo "$POETRY_INSTALL/bin" >> "$GITHUB_PATH"
 
 fi
