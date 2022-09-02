b970542cd55530723b6f944998cca7263460367f
Kenneth J. Pronovici <pronovic@ieee.org>
2022-09-02 14:59:47 -0500

    Fix location of poetry install and config on Windows

--- utils/poetrydirs.sh
+++ utils/poetrydirs.sh
@@ -14,8 +14,8 @@ if [ "$GITHUB_ACTIONS" == "true" ]; then
       echo "POETRY_INSTALL=/Users/runner/.local" >> "$GITHUB_ENV"
       echo "POETRY_CONFIG=/Users/runner/Library/Preferences/pypoetry" >> "$GITHUB_ENV"
    elif [ "$RUNNER_OS" == "Windows" ]; then
-      echo "POETRY_INSTALL=C:/Users/runneradmin/AppData/Roaming/Python" >> "$GITHUB_ENV"
-      echo "POETRY_CONFIG=C:/Users/runneradmin/AppData/Roaming/pypoetry" >> "$GITHUB_ENV"
+      echo "POETRY_INSTALL=C:\\Users\\runneradmin\\AppData\\Roaming\\Python\\Scripts" >> "$GITHUB_ENV"
+      echo "POETRY_CONFIG=C:\\Users\\runneradmin\\AppData\\Roaming\\pypoetry" >> "$GITHUB_ENV"
    fi 
 fi
 
