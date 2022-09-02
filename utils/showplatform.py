638a328fde64d2423121cddbf7e9df734f0b7e02
Kenneth J. Pronovici <pronovic@ieee.org>
2022-09-02 16:41:11 -0500

    Give up on caching poetry install

--- utils/showplatform.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Display information about the current platform, for use in GitHub Actions
-
-import platform
-import sys
-
-fields = ["   %s: %s" % i for i in zip(["system", "node", "release", "version", "machine", "processor"], platform.uname()) if i[1]]
-print("Python %s" % sys.version)
-print("Platform\n%s" % "\n".join(fields))
