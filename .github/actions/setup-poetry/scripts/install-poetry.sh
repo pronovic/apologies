406d9d470654c25b8d0ae24392598c05b5ae272b
Kenneth J. Pronovici <pronovic@ieee.org>
2022-10-16 18:53:06 +0000

    Continued development

--- .github/actions/setup-poetry/scripts/install-poetry.sh
+++ .github/actions/setup-poetry/scripts/install-poetry.sh
@@ -11,3 +11,11 @@ echo "POETRY_CACHE=$POETRY_CACHE"
 
 curl -sSL https://install.python-poetry.org | python -
 
+# On Windows, something gets screwed up with this soft link when restoring from cache.
+# Recreating it seems to resolve the problem, although it's not clear why.
+cd $POETRY_HOME/bin
+if [ -f poetry.exe ]; then
+  rm -f poetry.exe
+  ln -s ../venv/Scripts/poetry.exe
+fi
+
