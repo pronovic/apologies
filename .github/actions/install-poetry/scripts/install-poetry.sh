0c850343acccd00eff231d737e93ac7c7a3827af
Kenneth J. Pronovici <pronovic@ieee.org>
2022-10-16 15:42:11 +0000

    Continued development

--- .github/actions/install-poetry/scripts/install-poetry.sh
+++ .github/actions/install-poetry/scripts/install-poetry.sh
@@ -2,6 +2,7 @@
 # Install the Poetry build tool
 # This assumes that $POETRY_VERSION and $POETRY_HOME are set by the caller
 set -e
+echo "PYTHON_VERSION=$PYTHON_VERSION"
 echo "POETRY_VERSION=$POETRY_VERSION"
 echo "POETRY_HOME=$POETRY_HOME"
-curl -sSL https://install.python-poetry.org | python3 -
+curl -sSL https://install.python-poetry.org | python -
