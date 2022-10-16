7621a05a1973d48f8d47357ec8f24f34b37d02a5
Kenneth J. Pronovici <pronovic@ieee.org>
2022-10-16 17:13:30 +0000

    Continued development

--- .github/actions/setup-poetry/scripts/configure-poetry.sh
+++ .github/actions/setup-poetry/scripts/configure-poetry.sh
@@ -3,13 +3,13 @@
 
 set -e
 
-poetry config cache-dir "$POETRY_CACHE" --local
+poetry config cache-dir "$POETRY_CACHE"
 
-poetry config virtualenvs.create true --local
-poetry config virtualenvs.in-project true --local
+poetry config virtualenvs.create true
+poetry config virtualenvs.in-project true
 
 if [ ! -z "$MAX_WORKERS" ]; then
-  poetry config installer.max-workers "$MAX_WORKERS" --local
+  poetry config installer.max-workers "$MAX_WORKERS"
 fi
 
 poetry config --list
