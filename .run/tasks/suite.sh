341c65c683940a540fdb09f8c055a71fb022fc26
Kenneth J. Pronovici <pronovic@ieee.org>
2022-10-26 01:28:19 +0000

    Put back coverage, otherwise coveralls doesn't work

--- .run/tasks/suite.sh
+++ .run/tasks/suite.sh
@@ -9,7 +9,7 @@ task_suite() {
    run_task install
    run_task checks
    run_task build
-   run_task test
+   run_task test -c
    run_task docs
    run_task demo --players=3 --mode=ADULT --delay=0.02 --exit
 }
