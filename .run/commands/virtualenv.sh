d2501d19f773fbdef014772647d4050b6c0db067
Kenneth J. Pronovici <pronovic@users.noreply.github.com>
2022-12-30 11:16:48 -0600

    Convert to latest readthedocs.io standard (#52)

--- .run/commands/virtualenv.sh
+++ .run/commands/virtualenv.sh
@@ -2,7 +2,7 @@
 # Create and update the virtualenv, synchronizing it to versions in poetry.lock
 
 command_virtualenv() {
-   poetry install --sync
+   poetry install --sync --all-extras
    if [ $? != 0 ]; then
       echo "*** Failed to install the virtualenv"
       exit 1
