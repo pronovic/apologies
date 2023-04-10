e7770d72792e89c427189a8670626b12a51814f5
Kenneth J. Pronovici <pronovic@ieee.org>
2023-04-10 23:18:17 +0000

    Fix checktabs to be safe for file named '-'

--- .run/commands/checktabs.sh
+++ .run/commands/checktabs.sh
@@ -9,7 +9,7 @@ command_checktabs() {
    cd "$REPO_DIR" # we need to be in the root of the repo for 'git ls-files' to do what we need
 
    if git rev-parse --git-dir > /dev/null 2>&1; then
-      files="$(git ls-files | grep -v -x -F --file=".tabignore")"
+      files="$(git ls-files | grep -v '^-$' | grep -v -x -F --file=".tabignore")"
       if [ ! -z "$files" ]; then
          result=$(grep -l "$(printf '\t')" $files)
          if [ $? == 0 ]; then
