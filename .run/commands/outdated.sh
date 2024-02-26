674504a32da3919ea8815e6df1b4716b9a53560f
Kenneth J. Pronovici <pronovic@ieee.org>
2024-02-26 03:05:00 +0000

    Pull in latest version of run-script-framework

--- .run/commands/outdated.sh
+++ .run/commands/outdated.sh
@@ -4,6 +4,17 @@
 # See: https://github.com/python-poetry/poetry/issues/2684#issuecomment-1076560832
 
 command_outdated() {
+   echo "Updating dependencies with current constraints..."
+   echo ""
+   poetry update
+   if [ $? != 0 ]; then
+      echo "*** Failed to update dependencies"
+      exit 1
+   fi
+
+   echo ""
+   echo "Checking for outdated constraints..."
+   echo ""
    poetry show --outdated | grep --file=<(poetry show --tree | grep '^\w' | cut -d' ' -f1 | sed 's/.*/^&\\s/')
    if [ $? != 0 ]; then
       echo "*** Failed to check for outdated constraints"
