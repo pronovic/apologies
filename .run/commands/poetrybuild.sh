1144b326505d0c4e1a766efe7ee0b437be9226d6
Kenneth J. Pronovici <pronovic@ieee.org>
2025-01-21 17:09:55 -0600

    Pull in latest run-script-framework

--- .run/commands/poetrybuild.sh
+++ .run/commands/poetrybuild.sh
@@ -1,6 +1,13 @@
 # vim: set ft=bash ts=3 sw=3 expandtab:
 # Build release artifacts into the dist/ directory
 
+# Poetry v2 attaches the epoch timestamp (1970-01-01 00:00) to all of the files
+# in the sdist .tar.gz file, rather than the timestamp from the filesystem they
+# were sourced from, as was the behavior in Poetry v1.  To work around this, I'm
+# setting the $SOURCE_DATE_EPOCH to the current UTC epoch seconds value.
+# 
+# See also: https://github.com/python-poetry/poetry/issues/10083
+
 command_poetrybuild() {
    echo "Building release artifacts..."
 
@@ -8,7 +15,7 @@ command_poetrybuild() {
 
    poetry version
 
-   poetry build
+   SOURCE_DATE_EPOCH=$(TZ=UTC date "+%s") poetry build
    if [ $? != 0 ]; then
       echo "*** Build failed"
       exit 1
