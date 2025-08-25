e815e9c9981ac7ad37d97e4e23bdcdfa31875879
Kenneth J. Pronovici <pronovic@ieee.org>
2025-08-24 19:51:33 -0500

    Adjust comments

--- .run/commands/clean.sh
+++ .run/commands/clean.sh
@@ -1,10 +1,10 @@
 # vim: set ft=bash ts=3 sw=3 expandtab:
-# Clean the source tree, removiong the virtualenv and derived data
+# Clean the source tree, removing the virtualenv and other derived data.
 
 command_clean() {
    rm -rf .venv
    rm -rf dist
    rm -rf docs/_build
-   find . -name "__pycache__" | xargs rm -rf 
+   find . -name "__pycache__" -type d | xargs rm -rf
 }
 
